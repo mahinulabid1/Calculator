98891556cc3b8a3c79e612e6933b430f64a93132
MahinulA <himahinulabid@gmail.com>
2022-09-04 00:43:48 +0600

    Created History Open/close section

--- src/otherFunctionalities.js
+++ src/otherFunctionalities.js
@@ -1,2 +1,11 @@
 const HistoryButton = document.querySelector(".button-history");
+const HistorySection = document.querySelector(".history-section");
+const CloseHistoryBtn = document.querySelector(".close-history");
 
+HistoryButton.addEventListener("click",()=>{
+    HistorySection.classList.toggle("history-no-display");
+});
+
+CloseHistoryBtn.addEventListener("click",()=>{
+    HistorySection.classList.toggle("history-no-display");
+});
